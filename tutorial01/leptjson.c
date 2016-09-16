89f707d123631c7114c398d106c6169e0b5e533f
john jiang <nigelchiang@outlook.com>
2016-09-16 14:14:36 +0800

    Add boolean parse

--- tutorial01/leptjson.c
+++ tutorial01/leptjson.c
@@ -5,34 +5,100 @@
 #define EXPECT(c, ch)       do { assert(*c->json == (ch)); c->json++; } while(0)
 
 typedef struct {
-    const char* json;
-}lept_context;
+    const char *json;
+} lept_context;
 
-static void lept_parse_whitespace(lept_context* c) {
+//static的全局变量, 表示只有文件内部链接, 无法在其他文件引用, 相当于是这个文件的私有变量
+static void lept_parse_whitespace(lept_context *c) {
+
     const char *p = c->json;
-    while (*p == ' ' || *p == '\t' || *p == '\n' || *p == '\r')
+    while (*p == ' ' || *p == '\t' || *p == '\n' || *p == '\r') {
         p++;
+    }
     c->json = p;
 }
 
-static int lept_parse_null(lept_context* c, lept_value* v) {
+static int lept_parse_null(lept_context *c, lept_value *v) {
+
     EXPECT(c, 'n');
-    if (c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l')
+    if (c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l') {
         return LEPT_PARSE_INVALID_VALUE;
+    }
     c->json += 3;
     v->type = LEPT_NULL;
     return 0;
 }
 
-static int lept_parse_value(lept_context* c, lept_value* v) {
-    switch (*c->json) {
-        case 'n':  return lept_parse_null(c, v);
-        case '\0': return LEPT_PARSE_EXPECT_VALUE;
-        default:   return LEPT_PARSE_INVALID_VALUE;
+static int lept_parse_bool(lept_context *c, lept_value *v, const char *bool) {
+
+    char bool_type = bool[0];
+    EXPECT(c, *bool++);
+    const char *p = c->json;
+
+    while (*bool) {
+        if (*p++ != *bool++) {
+            return LEPT_PARSE_INVALID_VALUE;
+        }
     }
+    c->json = p;
+
+    if (bool_type == 't') {
+        v->type = LEPT_TRUE;
+    } else {
+        v->type = LEPT_FALSE;
+    }
+    return 0;
 }
 
-int lept_parse(lept_value* v, const char* json) {
+static int lept_parse_true(lept_context *c, lept_value *v) {
+    //内部改变一下函数的实现
+    return lept_parse_bool(c, v, "true");
+}
+
+static int lept_parse_false(lept_context *c, lept_value *v) {
+
+    EXPECT(c, 'f');
+    if (c->json[0] != 'a' || c->json[1] != 'l' || c->json[2] != 's' || c->json[3] != 'e') {
+        return LEPT_PARSE_INVALID_VALUE;
+    }
+    c->json += 4;
+    v->type = LEPT_FALSE;
+    return 0;
+}
+
+static int lept_parse_value(lept_context *c, lept_value *v) {
+
+    int r = 0;
+    switch (*c->json) {
+        case 'n':
+            r = lept_parse_null(c, v);
+            break;
+        case 't':
+            r = lept_parse_true(c, v);
+            break;
+        case 'f':
+            r = lept_parse_false(c, v);
+            break;
+        case '\0':
+            return LEPT_PARSE_EXPECT_VALUE;
+        default:
+            return LEPT_PARSE_INVALID_VALUE;
+    }
+
+    if (r != 0) {
+        return r;
+    }
+
+    lept_parse_whitespace(c);
+    if (*c->json != '\0') {
+        v->type = LEPT_NULL;
+        return LEPT_PARSE_ROOT_NOT_SINGULAR;
+    }
+    return r;
+}
+
+int lept_parse(lept_value *v, const char *json) {
+
     lept_context c;
     assert(v != NULL);
     c.json = json;
@@ -41,7 +107,8 @@ int lept_parse(lept_value* v, const char* json) {
     return lept_parse_value(&c, v);
 }
 
-lept_type lept_get_type(const lept_value* v) {
+lept_type lept_get_type(const lept_value *v) {
+
     assert(v != NULL);
     return v->type;
 }
--- tutorial01/test.c
+++ tutorial01/test.c
@@ -21,6 +21,7 @@ static int test_pass = 0;
 #define EXPECT_EQ_INT(expect, actual) EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%d")
 
 static void test_parse_null() {
+
     lept_value v;
     v.type = LEPT_FALSE;
     EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "null"));
@@ -28,6 +29,7 @@ static void test_parse_null() {
 }
 
 static void test_parse_expect_value() {
+
     lept_value v;
 
     v.type = LEPT_FALSE;
@@ -37,9 +39,14 @@ static void test_parse_expect_value() {
     v.type = LEPT_FALSE;
     EXPECT_EQ_INT(LEPT_PARSE_EXPECT_VALUE, lept_parse(&v, " "));
     EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+
+    v.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_EXPECT_VALUE, lept_parse(&v, "\t\n"));
+    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
 }
 
 static void test_parse_invalid_value() {
+
     lept_value v;
     v.type = LEPT_FALSE;
     EXPECT_EQ_INT(LEPT_PARSE_INVALID_VALUE, lept_parse(&v, "nul"));
@@ -48,23 +55,60 @@ static void test_parse_invalid_value() {
     v.type = LEPT_FALSE;
     EXPECT_EQ_INT(LEPT_PARSE_INVALID_VALUE, lept_parse(&v, "?"));
     EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+
+    v.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_INVALID_VALUE, lept_parse(&v, "trrue"));
+    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+
+    v.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_INVALID_VALUE, lept_parse(&v, "fblse"));
+    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
 }
 
 static void test_parse_root_not_singular() {
+
     lept_value v;
     v.type = LEPT_FALSE;
     EXPECT_EQ_INT(LEPT_PARSE_ROOT_NOT_SINGULAR, lept_parse(&v, "null x"));
     EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+
+    v.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_ROOT_NOT_SINGULAR, lept_parse(&v, "truea"));
+    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+
+    v.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_ROOT_NOT_SINGULAR, lept_parse(&v, "true\ta"));
+    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+}
+
+static void test_parse_true() {
+
+    lept_value v;
+    v.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "true"));
+    EXPECT_EQ_INT(LEPT_TRUE, lept_get_type(&v));
+}
+
+static void test_parse_false() {
+
+    lept_value v;
+    v.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "false"));
+    EXPECT_EQ_INT(LEPT_FALSE, lept_get_type(&v));
 }
 
 static void test_parse() {
+
     test_parse_null();
+    test_parse_true();
+    test_parse_false();
     test_parse_expect_value();
     test_parse_invalid_value();
     test_parse_root_not_singular();
 }
 
 int main() {
+
     test_parse();
     printf("%d/%d (%3.2f%%) passed\n", test_pass, test_count, test_pass * 100.0 / test_count);
     return main_ret;
